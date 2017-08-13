56d551c58461cbb00d4e269a95dee5eb03e90947
ramfay <ramisa.fayez@my.jcu.edu.au>
2017-08-13 22:13:39 +1000

    add movie quote from esotsm

---
+++ ProgrammingExercises/MovieQuote.java
@@ -0,0 +1,10 @@
+public class MovieQuote
+{
+    public static void main(String[] args)
+    {
+        System.out.println("How happy is the blameless vestal's lot!");
+        System.out.println("The world forgetting, by the world forgot");
+        System.out.println("Eternal sunshine of the spotless mind!");
+        System.out.println("Each pray'r accepted, and each wish resign'd.");
+    }
+}
